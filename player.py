5f1dc06fd1c24563f05560df640d47ed6933f7bd
Amaaz28 <121357781+Amaaz28@users.noreply.github.com>
2025-09-29 18:57:08 -0400

    Update player.py

--- player.py
+++ player.py
@@ -1,11 +1,15 @@
+#betting rules
+
 MAX_LINES = 3
 MAX_BET = 1000
 MIN_BET = 1
 
 class Player:
+    #ititializes starting balance
     def __init__(self, balance=0):
         self.balance = balance
 
+    #Asks the player to deposit money, must be positive integer. loops until valip input is entered.
     def deposit(self):
         while True:
             amount = input("Enter the amount you want to deposit: $")
@@ -13,7 +17,9 @@ class Player:
                 self.balance = int(amount)
                 break
             print("Please enter a valid amount greater than 0.")
+            
 
+    #lets payers bet on how many lines they want to bet on
     def get_number_of_lines(self):
         while True:
             lines = input(f"Enter the number of lines to bet on (1-{MAX_LINES}): ")
@@ -21,6 +27,7 @@ class Player:
                 return int(lines)
             print("Please enter a valid number of lines.")
 
+    #Players choose bet per line
     def get_bet(self):
         while True:
             bet = input(f"Enter the bet per line (${MIN_BET}-${MAX_BET}): $")
